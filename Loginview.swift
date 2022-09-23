48c833ea7314fb4ec811cea2ccddf58d61d76ae5
Joshua Peete <86421763+joshpeete@users.noreply.github.com>
2022-09-22 23:15:25 -0400

    updates to UI started on switch
    -switching to a toggle bar for registration and login 
    -added email keyboard for email address text entry
    -started on splashview for when the app is loading

---
+++ Loginview.swift
@@ -0,0 +1,20 @@
+//
+//  Loginview.swift
+//  Raga-Mania
+//
+//  Created by Joshua Peete on 9/22/22.
+//
+
+import SwiftUI
+
+struct Loginview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Loginview_Previews: PreviewProvider {
+    static var previews: some View {
+        Loginview()
+    }
+}
