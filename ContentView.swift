904c2736a44170fa30d2fd2ab358be150aaedc1d
Joshua Peete <86421763+joshpeete@users.noreply.github.com>
2022-09-23 13:39:27 -0400

    functionality added for toggle buttons
    User can now select between registration and login by using the toggle buttons at the top of the screen, page changes with the toggle button.

--- ContentView.swift
+++ ContentView.swift
@@ -10,13 +10,12 @@ struct ContentView: View {
     
     @State private var email=""
     @State private var password=""
-    @State private var userIsLoggedIn=false
     @State private var select=false
     
     var body: some View {
         NavigationView{
             ScrollView{
-                VStack{
+                VStack(spacing:12){
                     Picker(selection: $select, label: Text("Picker here")){
                         Text("Login")
                             .tag(true)
@@ -25,23 +24,28 @@ struct ContentView: View {
                             .tag(false)
                             .foregroundColor(.black)
                     }.pickerStyle(SegmentedPickerStyle())
-                    
-                    Button{
+                    if select{
+                        Button{
+                            
+                        }label: {
+                            Image(systemName: "person.fill")
+                                .font(.system(size: 75))
+                                .padding(12)
+                        }
                         
-                    }label: {
-                        Image(systemName: "person.fill")
-                            .font(.system(size: 75))
-                            .padding()
                     }
                     TextField("Email",text:$email)
                         .foregroundColor(.white)
                         .keyboardType(.emailAddress)
                         .autocapitalization(.none)
                         .textFieldStyle(.plain)
+                        .padding(12)
                         .placeholder(when: email.isEmpty){
                             Text("Email")
-                                .foregroundColor(.white)
+                                .foregroundColor(.black)
+                                .font(.system(size: 16))
                                 .bold()
+                                .padding(12)
                         }
                     Rectangle()
                         .frame(width: 350, height: 1)
@@ -50,19 +54,40 @@ struct ContentView: View {
                     SecureField("Password",text:$password)
                         .foregroundColor(.white)
                         .textFieldStyle(.plain)
+                        .padding(12)
                         .placeholder(when: password.isEmpty){
                             Text("Password")
-                                .foregroundColor(.white)
+                                .foregroundColor(.black)
                                 .bold()
+                                .font(.system(size: 16))
+                                .padding(12)
                         }
-                    
-                    Rectangle()
-                        .frame(width: 350, height: 1)
-                        .foregroundColor(.black)
-                }.navigationTitle("Create Account")
-                    .padding()
+                                        }
+                Rectangle()
+                    .frame(width: 350, height: 1)
+                    .foregroundColor(.black)
                 
-            }
+                Button{
+                    
+                }label: {
+                    HStack{
+                        Spacer()
+                        Text("Create Account")
+                            .foregroundColor(.white)
+                            .padding(.vertical,20)
+                            .font(.system(size: 24))
+                        Spacer()
+                    }.background(Color.blue)
+                        .frame(width: 250, height: 100, alignment: .center)
+                        .padding()
+                }
+                
+            }.navigationTitle(select ? "Log In" :" Create Account")
+                .padding()
+                .background(
+                        LinearGradient(gradient: Gradient(colors: [.yellow, .green]), startPoint: .top, endPoint: .bottom)
+                    )
+               
         }
         
     }
