9cbbb304a50596616a5b8d5ff3916ac9dfca3d34
nvaishu18 <85365310+nvaishu18@users.noreply.github.com>
2022-10-22 18:58:51 -0400

    Add files via upload
    vaishu's work 10/22/22

--- RMApp/RMApp/View/ContentView.swift
+++ RMApp/RMApp/View/ContentView.swift
@@ -1,4 +1,4 @@
-//STARTING PAGE
+//
 //  ContentView.swift
 //User registration and Login pages
 //Needs auth for email and password still
@@ -17,10 +17,10 @@ struct ContentView: View {
     @State private var password = ""
     @State private var select = false
     @State private var isActive = false
-    @State private var userIsLoggedIn = false
+    @ObservedObject var firebase = FirebaseInterface.instance
 
     var body: some View {
-        if userIsLoggedIn{
+        if firebase.userIsLoggedIn{
             JHomescreen()//if the user creates an account or logs in send them to the home page
         }else{
             NavigationView{
@@ -104,6 +104,26 @@ struct ContentView: View {
             
         }
     }
+  /*  Button{
+        handleAction()
+    }label: {
+        VStack{
+            Spacer()
+            Text(select ? "Forgot Password?")
+                .foregroundColor(.white)
+                .padding(.vertical,20)
+                .font(.system(size: 24, weight: .semibold))
+            Spacer()
+        }.background(Color.blue)
+            .frame(width: 250, height: 100, alignment: .center)
+            .padding()
+    }
+}.navigationTitle(select ? "Forgot Password?")
+    .padding()
+    .background(
+        LinearGradient(gradient: Gradient(colors: [.yellow, .green]), startPoint: .top, endPoint: .bottom)
+    )
+}*/
     func handleAction() {//links buttons to functions
         if select {
             loginUser()
@@ -111,27 +131,54 @@ struct ContentView: View {
             createNewAccount()
         }
     }
-    
+    //vaishu
     func loginUser() {//login func
-        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
-            if let error = error{
-                print(error.localizedDescription)
-            }
-            else if let user = user {
-                print(user)
-                userIsLoggedIn = true
-                isActive = false
-            }
+       Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            if error != nil {
+                //if there is an error, print "error"
+               print(error?.localizedDescription ?? "")
+           }
+           firebase.userIsLoggedIn = true
+           isActive = false
+           Playlist.instance.update()
         }
-    }
+   }
+    
+    
+   /* func forgotPassword() {//forgot pass
+       Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            if error != nil {
+                //if there is an error, print "error"
+               print(error?.localizedDescription ?? "")
+           }
+            firebase.userIsLoggedIn = true
+           isActive = false
+        }
+   }
+    Auth.auth().sendPasswordReset(withEmail: resetEmail!, completion: { (error) in
+       //Make sure you execute the following code on the main queue
+       DispatchQueue.main.async {
+           //Use "if let" to access the error, if it is non-nil
+           if let error = error {
+               let resetFailedAlert = UIAlertController(title: "Reset Failed", message: error.localizedDescription, preferredStyle: .alert)
+               resetFailedAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+               self.present(resetFailedAlert, animated: true, completion: nil)
+           } else {
+               let resetEmailSentAlert = UIAlertController(title: "Reset email sent successfully", message: "Check your email", preferredStyle: .alert)
+               resetEmailSentAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+               self.present(resetEmailSentAlert, animated: true, completion: nil)
+           }
+       }
+   })
+    */
     
     func createNewAccount() {//createacc func
-        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
            if error != nil {
                 print(error?.localizedDescription ?? "")
     }
             //add text pop-up that says user account created
-            //userIsLoggedIn = true
+            //firebase.userIsLoggedIn = true
             //isActive = false
         }
     }
