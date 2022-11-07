75dccb99e516bfaca4de7eec294ace6fcf723abc
nvaishu18 <85365310+nvaishu18@users.noreply.github.com>
2022-11-07 10:39:43 -0500

    Add files via upload
    Live recording gets saved on to the saved toggle feature

---
+++ ShazamRecognizer1.swift
@@ -0,0 +1,159 @@
+//SHAZAMKIT FUNCS AND SETUP
+//  SwiftUIView.swift
+//  RMApp
+//
+//  Created by Joshua Peete on 10/18/22.
+//
+
+import SwiftUI
+import ShazamKit
+import AVKit
+
+typealias RecordingCompletion = (URL) -> Void
+
+class ShazamRecognizer: NSObject, ObservableObject, SHSessionDelegate{
+    @Published var session = SHSession()
+    @Published var audioEngine = AVAudioEngine()
+    @Published var errorMsg = ""
+    @Published var showError = false
+    @Published var isRecording = false
+    var recordingCompletion: RecordingCompletion? = nil
+    var file: AVAudioFile? = nil
+
+    //Found Track
+    @Published var matchedTrack: Track!
+    
+    override init(){
+        super.init()
+        session.delegate = self
+    }
+    
+    func session( _ session: SHSession, didFind match: SHMatch){
+        //Match Found
+        if let firstItem = match.mediaItems.first{
+            //Shows what song is being recognized in the output window
+            DispatchQueue.main.async {
+                self.matchedTrack = Track(path: "", title: firstItem.title ?? "", artist: firstItem.artist ?? "", artwork: firstItem.artworkURL ?? URL(string: "")!)
+                
+                self.callCompletion()
+            }
+        }
+    }
+    
+    func session( _ session: SHSession, didNotFindMatchFor signature: SHSignature, error: Error?){
+        //No Match
+        DispatchQueue.main.async {
+            self.errorMsg = "\(error!)"
+            self.showError.toggle()
+            //stopping audio recording
+            self.stopRecording()
+            self.callCompletion()
+        }
+    }
+    
+    func stopRecording(){
+        audioEngine.stop()
+        withAnimation{
+            isRecording = false
+        }
+    }
+    
+    func callCompletion() {
+        guard let file = file else { return }
+        
+        recordingCompletion?(file.url)
+        recordingCompletion = nil
+        
+        do {
+            try FileManager.default.removeItem(at: file.url)
+            self.file = nil
+        } catch {
+            print(error)
+        }
+    }
+    
+    //Fetch Music
+    func listenMusic(completion: RecordingCompletion?){
+        recordingCompletion = completion
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        audioSession.requestRecordPermission{ status in
+            
+            if status{
+                self.recordAudio()
+            }
+            else{
+                self.errorMsg = "Please Allow Microphone Access"
+                self.showError.toggle()
+            }
+        }
+    }
+    
+    func recordAudio(){
+        //if already recording...stop
+        if audioEngine.isRunning{
+            self.stopRecording()
+            return
+        }
+        //recording Audio
+        let inputNode = audioEngine.inputNode
+        let format = inputNode.outputFormat(forBus: .zero)
+        
+        //removing if already installed
+        inputNode.removeTap(onBus: .zero)
+                
+        
+        //instslling when you tap the button
+        inputNode.installTap(onBus: .zero, bufferSize: 1024, format: format){ [weak self] (buffer, time) in
+            do {
+                if self?.file == nil {
+                    var url = try FileManager.default.url(for: .documentDirectory,
+                                                          in: .userDomainMask,
+                                                          appropriateFor: nil,
+                                                          create: true)
+                    
+                    let dateFormatter = DateFormatter()
+                    
+                    dateFormatter.dateStyle = .medium
+                    dateFormatter.timeStyle = .medium
+                    let name = dateFormatter.string(from: Date())
+                    
+                    url = url.appendingPathComponent(name)
+                    
+                    let bufferFormat = buffer.format
+
+                    self?.file = try AVAudioFile(forWriting: url,
+                                                 settings: bufferFormat.settings,
+                                                 commonFormat: bufferFormat.commonFormat,
+                                                 interleaved: bufferFormat.isInterleaved)
+                }
+                
+                try self?.file?.write(from: buffer)
+            } catch {
+                print(error)
+            }
+            
+            //ShazamKit Session Start
+            self?.session.matchStreamingBuffer(buffer, at: time)
+        }
+        //starting audio matching
+        audioEngine.prepare()
+        
+        do{
+            try audioEngine.start()
+            print("Starting")
+            withAnimation{
+                DispatchQueue.main.async {
+                    self.isRecording = true
+                }
+            }
+        }
+        catch{
+            self.errorMsg = error.localizedDescription
+            self.showError.toggle()
+        }
+    }
+}
+
+
+
