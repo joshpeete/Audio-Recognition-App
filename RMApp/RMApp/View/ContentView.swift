0edb8dfd49f4c1db1599ec2cbf41df4f9435d2de
joshpeete <joshuapeete1112@gmail.com>
2022-11-09 11:31:44 -0500

    Fixing broken xocde issues after IDE update

--- RMApp/RMApp/View/ContentView.swift
+++ RMApp/RMApp/View/ContentView.swift
@@ -6,7 +6,6 @@
 //
 //  Created by Joshua Peete on 9/21/22.
 //
-
 import SwiftUI
 import Firebase
 
@@ -187,35 +186,7 @@ func isValidEmail(_ email: String) -> Bool {
         }
    }
     
-    
-   /* func forgotPassword() {//forgot pass
-       Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
-            if error != nil {
-                //if there is an error, print "error"
-               print(error?.localizedDescription ?? "")
-           }
-            firebase.userIsLoggedIn = true
-           isActive = false
-        }
-   }
-    Auth.auth().sendPasswordReset(withEmail: resetEmail!, completion: { (error) in
-       //Make sure you execute the following code on the main queue
-       DispatchQueue.main.async {
-           //Use "if let" to access the error, if it is non-nil
-           if let error = error {
-               let resetFailedAlert = UIAlertController(title: "Reset Failed", message: error.localizedDescription, preferredStyle: .alert)
-               resetFailedAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-               self.present(resetFailedAlert, animated: true, completion: nil)
-           } else {
-               let resetEmailSentAlert = UIAlertController(title: "Reset email sent successfully", message: "Check your email", preferredStyle: .alert)
-               resetEmailSentAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-               self.present(resetEmailSentAlert, animated: true, completion: nil)
-           }
-       }
-   })
-    */
-    
-
+    func createNewAccount(){
         Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
             if error != nil {
                 print(error?.localizedDescription ?? "")
